40d52af5a69447540ef3cd5b14d20a8fc623cb3e
maggiejoe <maggiejoseph94@gmail.com>
2022-06-15 12:44:01 -0400

    all tests for employee pass

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,6 +1,23 @@
 const Employee = require('../lib/Employee');
 
+const employee = new Employee('Maggie', 2, 'maggie@email.com');
+
 test('Can create an object', () => {
-    const employee = new Employee('Maggie', 2, 'maggie@email.com');
     expect(typeof(employee)).toBe('object');
-})
+});
+
+test('Does getName show a name', () => {
+    expect(employee.getName()).toBe('Maggie');
+});
+
+test('Does getId show an id', () => {
+    expect(employee.getId()).toBe(2);
+});
+
+test('Does getEmail show an email', () => {
+    expect(employee.getEmail()).toBe('maggie@email.com');
+});
+
+test('Does getRole show employee', () => {
+    expect(employee.getRole()).toBe('Employee');
+});

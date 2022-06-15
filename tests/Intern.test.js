8a6718c79df3b835eedf8d27b2af84d60f307949
maggiejoe <maggiejoseph94@gmail.com>
2022-06-15 13:11:51 -0400

    intern tests all pass

---
+++ tests/Intern.test.js
@@ -0,0 +1,27 @@
+const Intern = require('../lib/Intern');
+
+const intern = new Intern('Gabriella', '011', 'gabriella@email.com', 'UoT');
+
+test('Can create an intern object', () => {
+    expect(typeof(intern)).toBe('object');
+});
+
+test('Does getName show a name', () => {
+    expect(intern.getName()).toBe('Gabriella');
+});
+
+test('Does getId show an id', () => {
+    expect(intern.getId()).toBe('011');
+});
+
+test('Does getemail show an email', () => {
+    expect(intern.getEmail()).toBe('gabriella@email.com');
+});
+
+test('Does getSchool show a school', () => {
+    expect(intern.getSchool()).toBe('UoT');
+});
+
+test('Does getRole show the Intern role', () => {
+    expect(intern.getRole()).toBe('Intern');
+});

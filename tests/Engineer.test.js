036f7a283ac64e2fa2fd5cc1eabea7b246ef69d6
maggiejoe <maggiejoseph94@gmail.com>
2022-06-15 12:56:46 -0400

    all engineer tests pass

---
+++ tests/Engineer.test.js
@@ -0,0 +1,27 @@
+const Engineer = require('../lib/Engineer');
+
+const engineer = new Engineer('Adrina', '001', 'adrina@email.com', 'adrinaGH');
+
+test('Can create an engineer object', () => {
+    expect(typeof(engineer)).toBe('object');
+});
+
+test('Does getName show a name', () => {
+    expect(engineer.getName()).toBe('Adrina');
+});
+
+test('Does getId show an id', () => {
+    expect(engineer.getId()).toBe('001');
+});
+
+test('Does getemail show an email', () => {
+    expect(engineer.getEmail()).toBe('adrina@email.com');
+});
+
+test('Does getGithub show a github account', () => {
+    expect(engineer.getGithub()).toBe('adrinaGH');
+});
+
+test('Does getRole show the Engineer role', () => {
+    expect(engineer.getRole()).toBe('Engineer');
+});
